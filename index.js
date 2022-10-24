1996accccaa2b7eadb8bca408a0c4dde76a95cee
Godzap <enzo.baiao1@hotmail.com>
2022-10-24 14:14:57 -0300

    Enviando dados do front pro back

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 // Avisando o express para usar o  EJS como view engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-
+// bodyparser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodYParser.json());
+// Rotas
 app.get("/",(req, res) => {
     res.render("index");
 });
@@ -15,4 +18,12 @@ app.get("/",(req, res) => {
 app.get("/perguntar",(req, res) =>{
     res.render("perguntar");
 })
+
+app.post("/salvarpergunta",(req, res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido" + titulo + "" + "Descricao" + descricao);
+
+
+});
 app.listen(8080,()=>{console.log("App rodando!");});
